b52f4a2cfa581783a9b03a89de2899878fcb033d
Xyz-King's <xyzkings22@gmail.com>
2026-02-03 21:43:11 +0800

    Auto upload via xyzup.py

---
+++ plugin/sound/sound010.js
@@ -0,0 +1,24 @@
+const axios = require("axios");
+const path = require("path");
+
+const BASE_RAW = "https://raw.githubusercontent.com/Leoo7z/Music/main";
+
+module.exports = {
+  command: ["sound010"],
+  category: "sound",
+  description: "Play sound dari GitHub RAW",
+
+  async execute(bot, m) {
+    const num = "10";
+    const url = `${BASE_RAW}/sound${num}.mp3`;
+
+    await bot.sendMessage(
+      m.key.remoteJid,
+      {
+        audio: { url },
+        mimetype: "audio/mpeg" // ✅ ini audio biasa
+      },
+      { quoted: m }
+    );
+  },
+};
