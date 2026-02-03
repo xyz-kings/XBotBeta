da45248d183d86e447fb0d26480781ddf9982fdd
Xyz-King's <xyzkings22@gmail.com>
2026-02-03 21:44:38 +0800

    Auto upload via xyzup.py

---
+++ plugin/sound/sad30.js
@@ -0,0 +1,24 @@
+const axios = require("axios");
+const path = require("path");
+
+const BASE_RAW = "https://raw.githubusercontent.com/Leoo7z/Music/main";
+
+module.exports = {
+  command: ["sad30"],
+  category: "sound",
+  description: "Play sad sound",
+
+  async execute(bot, m) {
+    const num = "30";
+    const url = `${BASE_RAW}/sad-music/sad${num}.mp3`;
+
+    await bot.sendMessage(
+      m.key.remoteJid,
+      {
+        audio: { url },
+        mimetype: "audio/mpeg" // ✅ ini audio biasa
+      },
+      { quoted: m }
+    );
+  },
+};
