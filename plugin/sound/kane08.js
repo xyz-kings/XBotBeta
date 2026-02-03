2efa3ef4c634934684d08d2933d4611fcce30013
Xyz-King's <xyzkings22@gmail.com>
2026-02-03 21:46:17 +0800

    Auto upload via xyzup.py

---
+++ plugin/sound/kane08.js
@@ -0,0 +1,24 @@
+const axios = require("axios");
+const path = require("path");
+
+const BASE_RAW = "https://raw.githubusercontent.com/Leoo7z/Music/main";
+
+module.exports = {
+  command: ["kane08"],
+  category: "sound",
+  description: "Play kane sound",
+
+  async execute(bot, m) {
+    const num = "8";
+    const url = `${BASE_RAW}/kene-music/kane${num}.mp3`;
+
+    await bot.sendMessage(
+      m.key.remoteJid,
+      {
+        audio: { url },
+        mimetype: "audio/mpeg" // ✅ ini audio biasa
+      },
+      { quoted: m }
+    );
+  },
+};
